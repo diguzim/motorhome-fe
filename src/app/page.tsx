8a565becca110091ea6d554d434d28719e186ee2
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2025-09-23 00:34:19 -0300

    Adding chadcdn button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <main>{/* Add your content here */}</main>;
+  return (
+    <main>
+      <h1>Exemplo de uso do Button (shadcn)</h1>
+      <Button>Botão padrão</Button>
+      <Button variant="outline">Botão Outline</Button>
+      <Button disabled>Botão Desabilitado</Button>
+    </main>
+  );
 }
